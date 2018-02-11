2210b13fe470287f5d39ee2841c8ee83f0c11b87
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-02-10 22:14:15 -0800

    Cleanup

--- src/main/java/com/khambay/alg/strings/StringReverse.java
+++ src/main/java/com/khambay/alg/strings/StringReverse.java
@@ -2,25 +2,24 @@ package com.khambay.alg.strings;
 
 public class StringReverse {
 
-    public String reverseGeek(String input) {
-        char[] temparray = input.toCharArray();
-        int left, right=0;
-        right = temparray.length-1;
+    public String reverse(String input) {
+        char[] strArray = input.toCharArray();
+        int left, right = 0;
+        right = strArray.length-1;
 
-        for (left=0; left < right ; left++ ,right--)
+        for (left=0; left < right ; left++,right--)
         {
-            // Swap values of left and right
-            char temp = temparray[left];
-            temparray[left] = temparray[right];
-            temparray[right]=temp;
+            char temp = strArray[left];
+            strArray[left] = strArray[right];
+            strArray[right]=temp;
         }
-        return new String(temparray);
+        return new String(strArray);
     }
 
     public static void main(String args[]) {
         String reverseString = "John";
 
         StringReverse stringReverse = new StringReverse();
-        System.out.println("Reversed stringGeek=" + stringReverse.reverseGeek(reverseString));
+        System.out.println("Reversed string=" + stringReverse.reverse(reverseString));
     }
 }
