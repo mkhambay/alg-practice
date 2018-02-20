88624c963f9c174a1f3f880051c7947054ede55e
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-02-19 16:40:17 -0800

    BST additions

--- src/main/java/com/khambay/alg/trees/TreeNode.java
+++ src/main/java/com/khambay/alg/trees/TreeNode.java
@@ -87,8 +87,45 @@ public class TreeNode {
         }
     }
 
+    public boolean isBST() {
+        if (left != null) {
+            if (data < left.data || !left.isBST()) {
+                return false;
+            }
+        }
+
+        if (right != null) {
+            if (data >= right.data || !right.isBST()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
     public void visit(TreeNode node) {
         System.out.print(node.data + " ");
+    }
+
+    public int height() {
+        int leftHeight = left != null ? left.height() : 0;
+        int rightHeight = right != null ? right.height() : 0;
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public static TreeNode createMinimalBST(int arr[], int start, int end){
+        if (end < start) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode n = new TreeNode(arr[mid]);
+        n.setLeftChild(createMinimalBST(arr, start, mid - 1));
+        n.setRightChild(createMinimalBST(arr, mid + 1, end));
+        return n;
+    }
+
+    public static TreeNode createMinimalBST(int array[]) {
+        return createMinimalBST(array, 0, array.length - 1);
     }
 
     public static void main(String[] args) {
