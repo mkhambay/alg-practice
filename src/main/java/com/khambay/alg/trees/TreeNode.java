45cbe1490fc48a766d704506cc9a16d0e031eff2
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-02-21 17:27:52 -0800

    Tree - Common Ancestor

--- src/main/java/com/khambay/alg/trees/TreeNode.java
+++ src/main/java/com/khambay/alg/trees/TreeNode.java
@@ -296,6 +296,36 @@ public class TreeNode {
         return n;
     }
 
+    //4.8 First Common Ancestor
+    public static TreeNode commonAncestor(TreeNode p, TreeNode q) {
+        int delta = depth(p) - depth(q); // get difference in depths
+        TreeNode first = delta > 0 ? q : p; // get shallower node
+        TreeNode second = delta > 0 ? p : q; // get deeper node
+        second = goUpBy(second, Math.abs(delta)); // move shallower node to depth of deeper
+        while (first != second && first != null && second != null) {
+            first = first.parent;
+            second = second.parent;
+        }
+        return first == null || second == null ? null : first;
+    }
+
+    public static TreeNode goUpBy(TreeNode node, int delta) {
+        while (delta > 0 && node != null) {
+            node = node.parent;
+            delta--;
+        }
+        return node;
+    }
+
+    public static int depth(TreeNode node) {
+        int depth = 0;
+        while (node != null) {
+            node = node.parent;
+            depth++;
+        }
+        return depth;
+    }
+
     public static void main(String[] args) {
         TreeNode tree = new TreeNode(1);
         tree.left = new TreeNode(2);
@@ -373,6 +403,12 @@ public class TreeNode {
                 System.out.println(node1.data + "->" + null);
             }
         }
+
+        TreeNode root2 = TreeNode.createMinimalBST(array);
+        TreeNode n3 = root2.find(3);
+        TreeNode n7 = root2.find(7);
+        TreeNode ancestor = commonAncestor(n3, n7);
+        System.out.println(ancestor.data);
     }
 }
 
