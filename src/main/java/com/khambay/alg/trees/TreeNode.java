f7eda705919a044d16355243809f310b77ab1348
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-02-21 17:00:54 -0800

    Tree - Successor

--- src/main/java/com/khambay/alg/trees/TreeNode.java
+++ src/main/java/com/khambay/alg/trees/TreeNode.java
@@ -264,6 +264,38 @@ public class TreeNode {
         return true;
     }
 
+    //4.6 Successor
+    public static TreeNode inorderSucc(TreeNode n) {
+        if (n == null) {
+            return null;
+        }
+
+        // Found right children -> return left most node of right subtree
+        if (n.parent == null || n.right != null) {
+            return leftMostChild(n.right);
+        }
+        else {
+            TreeNode q = n;
+            TreeNode x = q.parent;
+            // Go up until we�re on left instead of right
+            while (x != null && x.left != q) {
+                q = x;
+                x = x.parent;
+            }
+            return x;
+        }
+    }
+
+    public static TreeNode leftMostChild(TreeNode n) {
+        if (n == null) {
+            return null;
+        }
+        while (n.left != null) {
+            n = n.left;
+        }
+        return n;
+    }
+
     public static void main(String[] args) {
         TreeNode tree = new TreeNode(1);
         tree.left = new TreeNode(2);
@@ -329,6 +361,18 @@ public class TreeNode {
         System.out.println("should be false: " + condition);
         condition = checkBSTSolution2(node);
         System.out.println("should be false: " + condition);
+
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        TreeNode root1 = TreeNode.createMinimalBST(array);
+        for (int i = 0; i < array.length; i++) {
+            TreeNode node1 = root1.find(array[i]);
+            TreeNode next = inorderSucc(node1);
+            if (next != null) {
+                System.out.println(node1.data + "->" + next.data);
+            } else {
+                System.out.println(node1.data + "->" + null);
+            }
+        }
     }
 }
 
