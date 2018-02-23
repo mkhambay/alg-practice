87a7c12a98fd4c50f74959bfb96f4916afbbb11c
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-02-23 15:06:30 -0800

    Stack - Min Stack

---
+++ src/main/java/com/khambay/alg/stack/MinStack.java
@@ -0,0 +1,122 @@
+package com.khambay.alg.stack;
+
+public class MinStack {
+
+    public class Stack {
+
+        class Node {
+            int data;
+            Node next;
+
+            public Node(int data) {
+                this.data = data;
+            }
+        }
+
+        private Node top;
+
+        public Stack() {}
+
+        public Stack(int data) {
+            Node n = new Node(data);
+            top = n;
+        }
+
+        public void push(int data) {
+            Node n = new Node(data);
+            n.next = top;
+            top = n;
+        }
+
+        public int pop() throws Exception {
+            if(top == null) {
+                throw new Exception("Empty stack");
+            }
+            Node n = top;
+            top = top.next;
+            return n.data;
+        }
+
+        public int peek() throws Exception {
+            if(top == null) {
+                throw new Exception("Empty stack");
+            }
+            return top.data;
+        }
+
+        public boolean isEmpty() {
+            return top == null;
+        }
+
+        public void printStack() {
+            Node n = top;
+            System.out.print("Stack -> [");
+            while(n.next != null) {
+                System.out.print(n.data + ",");
+                n = n.next;
+            }
+            System.out.println(n.data + "]");
+        }
+    }
+
+    private Stack stackData = new Stack();
+    private Stack stackMin = new Stack();
+
+    public void push(int data) throws Exception {
+        if(data <= min()) {
+            stackMin.push(data);
+        }
+        stackData.push(data);
+    }
+
+    public int pop() throws Exception {
+        int popData = stackData.pop();
+        if(popData == min()) {
+            stackMin.pop();
+        }
+        return popData;
+    }
+
+    public int min() throws Exception {
+        if(stackMin.isEmpty()) {
+            return Integer.MAX_VALUE;
+        }
+        else {
+            return stackMin.peek();
+        }
+    }
+
+    public void printStackData() {
+        stackData.printStack();
+    }
+
+    public void printStackMin() {
+        stackMin.printStack();
+    }
+
+    public static void main(String[] args) throws Exception {
+        MinStack minStack = new MinStack();
+
+        minStack.push(6);
+        minStack.push(4);
+        minStack.push(3);
+        minStack.push(2);
+        minStack.push(8);
+        minStack.push(9);
+        minStack.push(1);
+        minStack.push(5);
+        minStack.printStackData();
+        minStack.printStackMin();
+
+        System.out.println("Pop");
+        minStack.pop();
+        minStack.printStackData();
+        minStack.printStackMin();
+
+        System.out.println("Pop min");
+        minStack.pop();
+        minStack.printStackData();
+        minStack.printStackMin();
+
+    }
+}
