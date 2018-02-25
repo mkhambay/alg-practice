02421cc22049f7dc3e6d8a9db768efbe2f26ee25
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-02-24 16:02:56 -0800

    Sort - Insertion Sort

---
+++ src/main/java/com/khambay/alg/sorting/InsertionSort.java
@@ -0,0 +1,41 @@
+package com.khambay.alg.sorting;
+
+
+public class InsertionSort {
+
+    public void insertionSort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
+            for(int j = i ; j > 0 ; j--) {
+                if(arr[j] < arr[j-1]){
+                    swap(arr, j, j-1);
+                }
+            }
+        }
+    }
+
+    private void swap(int[] arr, int left, int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+
+    public void printArray(int arr[]) {
+        System.out.print("[");
+        for(int i = 0; i < arr.length-1; i++) {
+            System.out.print(arr[i] + ",");
+        }
+        System.out.println(arr[arr.length-1] + "]");
+    }
+
+    public static void main(String args[]) {
+        InsertionSort insertionSort = new InsertionSort();
+        int arr[] = {10,2,8,5,7,4,3,9,1,6};
+        System.out.println("Original array");
+        insertionSort.printArray(arr);
+
+        insertionSort.insertionSort(arr);
+
+        System.out.println("Sorted array");
+        insertionSort.printArray(arr);
+    }
+}
