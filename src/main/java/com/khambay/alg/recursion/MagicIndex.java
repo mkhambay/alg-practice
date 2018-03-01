17c1ce530216b875caed1544b360ba8b02cc8c16
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-03-01 13:43:59 -0800

    Recursion - TripleStep and MagicIndex

---
+++ src/main/java/com/khambay/alg/recursion/MagicIndex.java
@@ -0,0 +1,63 @@
+package com.khambay.alg.recursion;
+
+public class MagicIndex {
+
+    public static int magicIndex(int[] array) {
+        return magicIndexBinarySearch(array, 0, array.length - 1);
+    }
+
+    public static int magicIndexBinarySearch(int[] array, int start, int end) {
+        if(start > end) {
+            return -1;
+        }
+
+        int mid = (start + end)/2;
+        if(array[mid] == mid) {
+            return mid;
+        }
+        else if(array[mid] > mid) {
+            //go left
+            return magicIndexBinarySearch(array, start, mid - 1);
+        }
+        else {
+            //go right
+            return magicIndexBinarySearch(array, mid + 1, end);
+        }
+    }
+
+    public static int magicIndexWithDuplicates(int[] array) {
+        return magicIndexWithDuplicates(array, 0, array.length - 1);
+    }
+
+    public static int magicIndexWithDuplicates(int[] array, int start, int end) {
+        if (end < start) {
+            return -1;
+        }
+        int midIndex = (start + end) / 2;
+        int midValue = array[midIndex];
+        if (midValue == midIndex) {
+            return midIndex;
+        }
+        /* Search left */
+        int leftIndex = Math.min(midIndex - 1, midValue);
+        int left = magicIndexWithDuplicates(array, start, leftIndex);
+        if (left >= 0) {
+            return left;
+        }
+
+        /* Search right */
+        int rightIndex = Math.max(midIndex + 1, midValue);
+        int right = magicIndexWithDuplicates(array, rightIndex, end);
+
+        return right;
+    }
+
+
+    public static void main(String[] args) {
+        int[] arr = {-40, -20, -1, 1, 2, 3, 5, 7, 9, 12, 13};
+        System.out.println("Magic Index = " + magicIndex(arr));
+
+        int[] arr1 = {-10, -5, 2, 2, 2, 3, 4, 7, 9, 12, 13};
+        System.out.println("Magic Index with duplicates = " + magicIndexWithDuplicates(arr1));
+    }
+}
---
+++ src/main/java/com/khambay/alg/recursion/TripleStep.java
@@ -0,0 +1,42 @@
+package com.khambay.alg.recursion;
+
+import java.util.Arrays;
+
+public class TripleStep {
+
+    public static int countWays(int n) {
+        //Array from 1-50 and not 0-40. Thats why new int[n + 1]
+        int[] map = new int[n + 1];
+        Arrays.fill(map, -1);
+        return countWays(n, map);
+    }
+
+    public static int countWays(int n, int[] memo) {
+        if (n < 0) {
+            return 0;
+        }
+        else if (n == 0) {
+            return 1;
+        }
+        else if (memo[n] > -1) {
+            return memo[n];
+        }
+        else {
+            int one = countWays(n - 1, memo);
+            int two = countWays(n - 2, memo);
+            int three = countWays(n - 3, memo);
+
+            memo[n] = one + two + three;
+//                    countWays(n - 1, memo) +
+//                            countWays(n - 2, memo) +
+//                            countWays(n - 3, memo);
+            return memo[n];
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int ways = countWays(n);
+        System.out.println(ways);
+    }
+}
