bb6ac5c9b1e89b8a26625982b237a1e1efdc690e
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-03-12 17:42:09 -0700

    Added check for special characters

--- src/main/java/com/khambay/alg/strings/Palindrome.java
+++ src/main/java/com/khambay/alg/strings/Palindrome.java
@@ -12,6 +12,8 @@ public class Palindrome {
    * @return boolean - if its a palindrome
    */
   public boolean isPalindrome(String stringToCheck) {
+    stringToCheck = stringToCheck.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
     for(int i = 0, j = stringToCheck.length()-1; i < j; i++, j--) {
       if(stringToCheck.charAt(i) != stringToCheck.charAt(j)) {
         return false;
@@ -23,6 +25,7 @@ public class Palindrome {
   public static void main(String args[]) {
     String palindromeString = "madam";
     String nonPalindromeString = "blah";
+    String palindromeWithExtra = "A man, a plan, a canal: Panama";
 
     Palindrome palindrome = new Palindrome();
 
@@ -30,5 +33,7 @@ public class Palindrome {
         + palindrome.isPalindrome(palindromeString));
     System.out.println("Result for nonPalindromeString=" + nonPalindromeString + " is palindrome="
         + palindrome.isPalindrome(nonPalindromeString));
+    System.out.println("Result for palindromeWithExtra=" + palindromeWithExtra + " is palindrome="
+            + palindrome.isPalindrome(palindromeWithExtra));
   }
 }
