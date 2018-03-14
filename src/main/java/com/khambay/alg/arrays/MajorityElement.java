56dfdfb500e467cec188e21fdee1c541a795e205
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-03-14 09:54:15 -0700

    Arrays - Majority Element

---
+++ src/main/java/com/khambay/alg/arrays/MajorityElement.java
@@ -0,0 +1,62 @@
+package com.khambay.alg.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of size n, find the majority element.
+ * The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * You may assume that the array is non-empty and the majority element always exist in the array.
+ *
+ * Time - O(nlogn)
+ * Space - O(1)
+ */
+public class MajorityElement {
+
+    /*
+        If the elements are sorted in monotonically increasing (or decreasing) order,
+        the majority element can be found at index n/2 if n is odd and n/2 + 1 if n is even. */
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+
+    //////// Time and Space O(n)
+
+    private static Map<Integer, Integer> countNums(int[] nums) {
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            if (!counts.containsKey(num)) {
+                counts.put(num, 1);
+            }
+            else {
+                counts.put(num, counts.get(num)+1);
+            }
+        }
+        return counts;
+    }
+
+    public static int majorityElementMap(int[] nums) {
+        Map<Integer, Integer> counts = countNums(nums);
+
+        Map.Entry<Integer, Integer> majorityEntry = null;
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            if (majorityEntry == null || entry.getValue() > majorityEntry.getValue()) {
+                majorityEntry = entry;
+            }
+        }
+
+        return majorityEntry.getKey();
+    }
+
+    ////////
+
+    public static void main(String[] args) {
+        int[] nums = {2,1,2,5,2,2,4,2,6,3};
+        System.out.println("Majority Element Sort = " + majorityElement(nums));
+
+        System.out.println("Majority Element Map = " + majorityElementMap(nums));
+    }
+}
