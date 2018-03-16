e10c9f3dd65d424c25392e1e26b45be4fde2fb45
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-03-15 17:58:32 -0700

    Array - Pascals Triangle

---
+++ src/main/java/com/khambay/alg/arrays/PascalsTriangle.java
@@ -0,0 +1,48 @@
+package com.khambay.alg.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Time and Space - O(n^2)
+ */
+public class PascalsTriangle {
+
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        if(numRows == 0) {
+            return triangle;
+        }
+
+        //Row 0
+        List<Integer> rowOne = new ArrayList<>();
+        rowOne.add(1);
+        triangle.add(rowOne);
+
+        for(int rowNum = 1; rowNum < numRows; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum - 1);
+
+            //first element
+            row.add(1);
+
+            for(int j = 1; j < rowNum; j++) { //will not enter here for row 1
+                row.add(prevRow.get(j-1) + prevRow.get(j));
+            }
+
+            //last element
+            row.add(1);
+            triangle.add(row);
+        }
+        return triangle;
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = generate(5);
+
+        for(List<Integer> row : triangle) {
+            System.out.println(row);
+        }
+    }
+}
