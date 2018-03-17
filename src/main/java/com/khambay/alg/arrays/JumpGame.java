063828f1f4c1dde4e4137a78f9b19588b83e47e5
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-03-17 11:31:20 -0700

    Array - Jump Game

---
+++ src/main/java/com/khambay/alg/arrays/JumpGame.java
@@ -0,0 +1,26 @@
+package com.khambay.alg.arrays;
+
+/**
+ * Time - O(n)
+ * Space - O(1)
+ */
+public class JumpGame {
+
+    public static boolean canJump(int[] nums) {
+        int lastPos = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i + nums[i] >= lastPos) {
+                lastPos = i;
+            }
+        }
+        return lastPos == 0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        int[] nums1 = {3,2,1,0,4};
+
+        System.out.println("Can Jump {2,3,1,1,4} = " + canJump(nums));
+        System.out.println("Can Jump {3,2,1,0,4} = " + canJump(nums1));
+    }
+}
