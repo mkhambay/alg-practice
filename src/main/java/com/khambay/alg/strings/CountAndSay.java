6a851cd1680bc20f6f3853b472861863e2929301
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-03-19 08:18:44 -0700

    String - Count And Say

---
+++ src/main/java/com/khambay/alg/strings/CountAndSay.java
@@ -0,0 +1,32 @@
+package com.khambay.alg.strings;
+
+/**
+ * Time - O(n^2)
+ * Space - O(n)
+ */
+public class CountAndSay {
+
+    public static String countAndSay(int n) {
+        String s = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j <= s.length(); j++) {
+                if (j == s.length() || s.charAt(j - 1) != s.charAt(j)) {
+                    sb.append(count);
+                    sb.append(s.charAt(j - 1));
+                    count = 1;
+                } else {
+                    count++;
+                }
+            }
+            s = sb.toString();
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println("21 = " + countAndSay(n));
+    }
+}
