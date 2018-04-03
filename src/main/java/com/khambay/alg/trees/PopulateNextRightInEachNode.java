363daf9bbfe8b946935e3523ff0589f173616829
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-04-02 17:10:33 -0700

    Tree - PopulateNextRightInEachNode

---
+++ src/main/java/com/khambay/alg/trees/PopulateNextRightInEachNode.java
@@ -0,0 +1,45 @@
+package com.khambay.alg.trees;
+
+/**
+ * Time - O(n)
+ * Space - O(n) worst if tree is linear (height of tree)
+ */
+public class PopulateNextRightInEachNode {
+
+    public static class TreeLinkNode {
+        int val;
+        TreeLinkNode left, right, next;
+
+        TreeLinkNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void connect(TreeLinkNode root) {
+        if(root == null)
+            return;
+
+        if(root.left != null) {
+            root.left.next = root.right;
+        }
+        if(root.next != null && root.right != null) {
+            root.right.next = root.next.left;
+        }
+
+        connect(root.left);
+        connect(root.right);
+    }
+
+    public static void main(String[] args) {
+        TreeLinkNode tree = new TreeLinkNode(1);
+        tree.left = new TreeLinkNode(2);
+        tree.right = new TreeLinkNode(3);
+        tree.left.left = new TreeLinkNode(4);
+        tree.left.right = new TreeLinkNode(5);
+        tree.right.left = new TreeLinkNode(6);
+        tree.right.right = new TreeLinkNode(7);
+
+        connect(tree);
+        System.out.println("Next of 2 = " + tree.left.next.val);
+    }
+}
