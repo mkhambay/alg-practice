d425060e479abd79b6aeb1255a8d6f75d610c199
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-04-24 14:52:27 -0700

    Math - ReverseInteger

---
+++ src/main/java/com/khambay/alg/math/ReverseInteger.java
@@ -0,0 +1,30 @@
+package com.khambay.alg.math;
+
+/**
+ * Time and Space - O(n) ?
+ */
+public class ReverseInteger {
+
+    public static int reverse(int x) {
+        int result = 0;
+
+        while (x != 0) {
+            int lastDigit = x % 10;
+            int newResult = result * 10 + lastDigit;
+
+            if ((newResult - lastDigit) / 10 != result) {
+                return 0;
+            }
+
+            result = newResult;
+            x = x / 10;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Reverse 123 = " + reverse(123));
+        System.out.println("Reverse -123 = " + reverse(-123));
+    }
+}
