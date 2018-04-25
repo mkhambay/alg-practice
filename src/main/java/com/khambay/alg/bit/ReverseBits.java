4424b60cd2bce46a16c30b48e2182f00d058ac80
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-04-25 16:40:56 -0700

    Bit - Reverse Bits

---
+++ src/main/java/com/khambay/alg/bit/ReverseBits.java
@@ -0,0 +1,23 @@
+package com.khambay.alg.bit;
+
+/**
+ * Time O(n)
+ * Space O(1)
+ */
+public class ReverseBits {
+
+    public static int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result += n & 1;    // apply 1s mask gets the bit set either 0 & 1 = 0 or 1 & 1 = 1
+            n >>>= 1;           // do unsigned shift to right by 1
+            if (i < 31)         // for last digit, don't shift
+                result <<= 1;   // shift result left by 1
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("43261596 = " + reverseBits(43261596));
+    }
+}
