56e0cd39b2cb30429d87eed81ec7b02fa5cf16e9
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-04-27 05:50:49 -0700

    Hashtable - LengthOfLongestSubstring

---
+++ src/main/java/com/khambay/alg/hashtable/LengthOfLongestSubstring.java
@@ -0,0 +1,30 @@
+package com.khambay.alg.hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Time and Space - O(n)
+ */
+public class LengthOfLongestSubstring {
+
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("abcabcbb = " + lengthOfLongestSubstring("abcabcbb"));
+        System.out.println("bbbbb = " + lengthOfLongestSubstring("bbbbb"));
+        System.out.println("pwwkew = " + lengthOfLongestSubstring("pwwkew"));
+    }
+}
