b0aa23dc4998a28396be44606841137a31c3e40b
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-05-01 07:30:22 -0700

    Comments

--- src/main/java/com/khambay/alg/hashtable/LengthOfLongestSubstring.java
+++ src/main/java/com/khambay/alg/hashtable/LengthOfLongestSubstring.java
@@ -13,7 +13,7 @@ public class LengthOfLongestSubstring {
         Map<Character, Integer> map = new HashMap<>(); // current index of character
         // try to extend the range [i, j]
         for (int j = 0, i = 0; j < n; j++) {
-            if (map.containsKey(s.charAt(j))) {
+            if (map.containsKey(s.charAt(j))) { //found repeating char, reset i
                 i = Math.max(map.get(s.charAt(j)), i);
             }
             ans = Math.max(ans, j - i + 1);
