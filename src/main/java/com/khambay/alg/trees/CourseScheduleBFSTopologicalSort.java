6c9f579b6053a4578c64316121280bf9cf1a60ee
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-05-09 07:09:22 -0700

    Comments

--- src/main/java/com/khambay/alg/trees/CourseScheduleBFSTopologicalSort.java
+++ src/main/java/com/khambay/alg/trees/CourseScheduleBFSTopologicalSort.java
@@ -14,13 +14,13 @@ public class CourseScheduleBFSTopologicalSort {
 
         //build adj matrix
         for (int k = 0; k < prerequisites.length; k++) {
-            int i = prerequisites[k][1];
+            int i = prerequisites[k][1]; //prerequisite is in column 1
             int j = prerequisites[k][0];
 
-            if (edgeMatrix[i][j] == 0) {
+            if (edgeMatrix[i][j] == 0) { //makes sure no duplicates and same edge not counted twice
                 incomingEdge[j]++;
             }
-            edgeMatrix[i][j] = 1;
+            edgeMatrix[i][j] = 1; //add the edge
         }
 
         //build queue of nodes with no edges
@@ -33,9 +33,11 @@ public class CourseScheduleBFSTopologicalSort {
 
         int courseCount = 0;
         //process each node with no edge
+        //If nodesWithNoEdge has no such nodes (nodes with no edge to it), its not a DAG. There are cycles
+        //and we should normally throw an error and not process further ex 1,0 and 0,1
         while (!nodesWithNoEdge.isEmpty()) {
             int i = nodesWithNoEdge.poll(); //remove a node i from S
-            courseCount++;
+            courseCount++; //course can be taken as there is no prerequisite for it
 
             //go through adj matrix and start removing edges from i -> j
             for (int j = 0; j < edgeMatrix[0].length; j++) {
@@ -52,21 +54,21 @@ public class CourseScheduleBFSTopologicalSort {
 
 
     public static void main(String[] args) {
-        int[][] prerequisites = {{1,0}};
-        System.out.println("Can take 2 courses with prerequisites = {{1,0}} ?? "
-                + canFinish(2, prerequisites));
-
+//        int[][] prerequisites = {{1,0}};
+//        System.out.println("Can take 2 courses with prerequisites = {{1,0}} ?? "
+//                + canFinish(2, prerequisites));
+//
 //        int[][] prerequisites1 = {{1,0}, {0,1}};
 //        System.out.println("Can take 2 courses with prerequisites = {{1,0}, {0,1}} ?? "
 //                + canFinish(2, prerequisites1));
 
-//        int[][] prerequisites2 = {
-//                {11,5}, {11,7},
-//                {8,7}, {8,3},
-//                {2,11},
-//                {9,11}, {9,8},
-//                {10,11}, {10,3}
-//        };
-//        System.out.println("Can take 12 courses with prerequisites2 = " + canFinish(12, prerequisites2));
+        int[][] prerequisites2 = {
+                {11,5}, {11,7},
+                {8,7}, {8,3},
+                {2,11},
+                {9,11}, {9,8},
+                {10,11}, {10,3}
+        };
+        System.out.println("Can take 12 courses with prerequisites2 = " + canFinish(12, prerequisites2));
     }
 }
