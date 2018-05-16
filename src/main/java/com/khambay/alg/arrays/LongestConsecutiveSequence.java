8211d19f308b3943a641991bdc396c40761ff6f5
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-05-15 17:15:17 -0700

    Array - LongestConsecutiveSequence

---
+++ src/main/java/com/khambay/alg/arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,41 @@
+package com.khambay.alg.arrays;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Time and Space O(n)
+ */
+public class LongestConsecutiveSequence {
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> num_set = new HashSet<>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num-1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum+1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        return longestStreak;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {100, 4, 200, 1, 3, 2};
+        System.out.println("longestConsecutive = " + longestConsecutive(nums));
+    }
+}
