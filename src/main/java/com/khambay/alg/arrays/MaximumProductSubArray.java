4a865977941d7d4e8b89b5df615a1230ecbb4f5c
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-05-18 06:46:38 -0700

    Arrays - MaximumProductSubArray

---
+++ src/main/java/com/khambay/alg/arrays/MaximumProductSubArray.java
@@ -0,0 +1,33 @@
+package com.khambay.alg.arrays;
+
+/**
+ * Time - O(n)
+ * Space - O(1)
+ */
+public class MaximumProductSubArray {
+
+    public static int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int max = nums[0], min = nums[0], maxProduct = nums[0];
+
+        // we have to compare among max * A[i], min * A[i] as well as A[i],
+        // since this is product, a negative * negative could be positive
+        for (int i = 1; i < nums.length; i++) {
+            int prevMax = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(prevMax * nums[i], min * nums[i]), nums[i]);
+
+            maxProduct = Math.max(maxProduct, max);
+        }
+        return maxProduct;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,-2,4};
+        System.out.println("Max Product {2,3,-2,4} = " + maxProduct(nums));
+        int[] nums1 = {-2,1,-4};
+        System.out.println("Max Product {-2,1,-4} = " + maxProduct(nums1));
+    }
+}
