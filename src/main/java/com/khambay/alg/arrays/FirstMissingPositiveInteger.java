301868d25b8f875eab85965cfa770fc95f4c2fb7
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-05-18 08:03:59 -0700

    Arrays - FirstMissingPositiveInteger

---
+++ src/main/java/com/khambay/alg/arrays/FirstMissingPositiveInteger.java
@@ -0,0 +1,52 @@
+package com.khambay.alg.arrays;
+
+/**
+ * First Missing Smallest Positive Integer
+ * Time - O(n)
+ * Space - O(1)
+ */
+public class FirstMissingPositiveInteger {
+
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+
+        //sort
+        while (i < nums.length) {
+            if (nums[i] == i + 1 || nums[i] <= 0 || nums[i] > nums.length) {
+                i++;
+            }
+            else if (nums[nums[i] - 1] != nums[i]) { //put the number in its correct index
+                swap(nums, i, nums[i] - 1);
+            }
+            else {
+                i++;
+            }
+        }
+
+        //find the missing number = index + 1
+        i = 0;
+        while (i < nums.length && nums[i] == i + 1) {
+            i++;
+        }
+
+        return i + 1;
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,0};
+        System.out.println("Missing Number {1,2,0} = " + firstMissingPositive(nums));
+
+        int[] nums1 = {3,4,-1,1};
+        System.out.println("Missing Number {3,4,-1,1} = " + firstMissingPositive(nums1));
+
+        int[] nums2 = {7,8,9,11,12};
+        System.out.println("Missing Number {7,8,9,11,12} = " + firstMissingPositive(nums2));
+
+    }
+}
