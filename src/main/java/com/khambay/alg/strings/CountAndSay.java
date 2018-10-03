8629e61f2098fac1b87ed1dd58dc95c99e530cfd
Mandeep Khambay <mandeep.khambay@gmail.com>
2018-10-03 06:22:19 -0700

    Simplified Count And Say

--- src/main/java/com/khambay/alg/strings/CountAndSay.java
+++ src/main/java/com/khambay/alg/strings/CountAndSay.java
@@ -1,7 +1,7 @@
 package com.khambay.alg.strings;
 
 /**
- * Similar to String Compression
+ * Similar to String Compression - count goes first
  * Time - O(n^2)
  * Space - O(n)
  */
@@ -9,22 +9,29 @@ public class CountAndSay {
 
     public static String countAndSay(int n) {
         String s = "1";
+
         for (int i = 1; i < n; i++) {
-            StringBuilder sb = new StringBuilder();
-            int count = 1;
-            for (int j = 1; j <= s.length(); j++) {
-                if (j == s.length() //also needed to kick off when String only has "1" in it
-                        || s.charAt(j - 1) != s.charAt(j)) {
-                    sb.append(count);
-                    sb.append(s.charAt(j - 1));
-                    count = 1;
-                } else {
-                    count++;
-                }
-            }
-            s = sb.toString();
+            s = compress(s);
         }
         return s;
+    }
+
+    private static String compress(String str) {
+        StringBuilder compressed = new StringBuilder();
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            count++;
+
+            if(i + 1 == str.length() //at the last character
+                    || str.charAt(i) != str.charAt(i + 1) //peek ahead
+                    ) {
+                compressed.append(count); //count goes first
+                compressed.append(str.charAt(i));
+                count = 0;
+            }
+        }
+        return compressed.toString();
     }
 
     public static void main(String[] args) {
